157edffc203bdded35304ca35cbaac1a78e3be31
Caio Lagreca <caiomiranda701@hotmail.com>
2024-08-25 19:37:50 +1000

    first commit

---
+++ Data/Migrations/20240825093225_InitialMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextEditor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Context",
+                table: "Docs",
+                newName: "Content");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Docs",
+                newName: "Context");
+        }
+    }
+}
